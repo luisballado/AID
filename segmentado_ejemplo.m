6deea59eee2557f21a8d173c2f7e28f68e12230c
Luis Ballado <gianna@MacBook-Pro-de-Luis.local>
2023-07-30 22:05:50 -0600

    script

--- segmentado_ejemplo.m
+++ segmentado_ejemplo.m
@@ -5,56 +5,51 @@ warning off
 set(0,'defaultTextFontName','Courier')
 
 %leer la imagen
-original = imread('/Users/gianna/Documents/CINVESTAV/AID/img2/todo.png');
+original = imread('/Users/gianna/Documents/CINVESTAV/AID/img2/todos_blanco2.png');
 
 %pasar la imagen a espacio de grises
 x = rgb2gray(original);
-
-%mostrar imagen
-%figure;
-%imshow(x);
-%title('Original Image');
 
 %--------------------------------------------
 % detectar bordes
 %--------------------------------------------
 edge_detected = edge(x,'canny');
-figure;
-imshow(edge_detected);
-title('Bordes canny');
+%figure;
+%imshow(edge_detected);
+%title('Bordes canny');
 
 %cerrar la imagen de bordes
 cerrar = imclose(edge_detected,strel('square',4));
-figure;
-imshow(cerrar);
-title('Cerrar imagen1');
+%figure;
+%imshow(cerrar);
+%title('Cerrar imagen1');
 
 %Hacer bwopen
 %
 mask_image = bwareaopen(cerrar,80);
-figure;
-imshow(mask_image);
-title('after a bwareaopen');
+%figure;
+%imshow(mask_image);
+%title('after a bwareaopen');
 
 %__
 cerrar_h = imfill(mask_image,'holes');
-figure;
-imshow(cerrar_h);
-title('Fill image');
+%figure;
+%imshow(cerrar_h);
+%title('Fill image');
 %__
 
 
 %cerrar la imagen de bordes
 cerrar2 = imclose(cerrar_h,strel('line',3,90));
-figure;
-imshow(cerrar2);
-title('Cerrar imagen1');
+%figure;
+%imshow(cerrar2);
+%title('Cerrar imagen1');
 
 %__
 cerrar22 = imfill(cerrar2,'holes');
-figure;
-imshow(cerrar22);
-title('Fill image');
+%figure;
+%imshow(cerrar22);
+%title('Fill image');
 %__
 
 %
@@ -62,29 +57,29 @@ title('Fill image');
 %Hacer bwopen
 %
 mask_image = bwareaopen(cerrar22,1000);
-figure;
-imshow(mask_image);
-title('after a bwareaopen');
+%figure;
+%imshow(mask_image);
+%title('after a bwareaopen');
 
 %cerrar la imagen de bordes
 cerrar = imclose(mask_image,strel('octagon',9));
-figure;
-imshow(cerrar);
-title('Cerrar imagen1');
+%figure;
+%imshow(cerrar);
+%title('Cerrar imagen1');
 
 %__
 cerrar2 = imfill(cerrar,'holes');
-figure;
-imshow(cerrar2);
-title('Fill image');
+%figure;
+%imshow(cerrar2);
+%title('Fill image');
 %__
 
 
 %EROSIONAR
 abrir = imerode(cerrar2,strel('line',2,180));
-figure;
-imshow(abrir);
-title('Imerode');
+%figure;
+%imshow(abrir);
+%title('Imerode');
 
 %%%
 
@@ -105,13 +100,38 @@ title('after a bwareaopen');
 %imshow(mask_image22);
 %title('after a imclose con disco 8');
 
-title('MASK Image');
+
 red = original(:,:,1).*uint8(mask_image);
 green = original(:,:,2).*uint8(mask_image);
 blue = original(:,:,3).*uint8(mask_image);
 op = cat(3,red,green,blue);
 figure;
 imshow(op);
+title('MASK Image');
+
+imshowpair(original,mask_image,'montage');
+%imshowpair(original,mask_image,'montage');
+
+[L,num]=bwlabel(mask_image,4);
+disp('Numero de objetos')
+disp(num)
 
 
+Iregion = regionprops(mask_image,'centroid');
 
+stats = regionprops(L,'Eccentricity','Area','BoundingBox');
+areas = [stats.Area];
+eccentricities = [stats.Eccentricity];
+
+idxOfSkittles = find(eccentricities);
+statsDefects = stats(idxOfSkittles);
+
+figure, imshow(x);
+hold on;
+
+for idx = 1: length(idxOfSkittles)
+    h = rectangle('Position', statsDefects(idx).BoundingBox, 'EdgeColor', 'r', 'LineWidth', 2);
+end
+
+title(['Hay ',num2str(num), ' objetos']);
+hold off;
