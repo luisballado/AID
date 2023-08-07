1c5a51522d197e5c1114da72c9f4be1976a0f49c
Luis Ballado <luisalbertoballadoaradias@iMac-de-LTI-9-Luis.local>
2023-08-07 17:36:36 -0600

    data aumentation

--- images/train/data_aumentation.py
+++ images/train/data_aumentation.py
@@ -9,7 +9,10 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot
 
 # we first load the image
-image = load_img('m_1.png')
+file_name = 'llave_8_v1'
+file_folder = 'llave_perica'
+
+image = load_img(file_folder+'/'+file_name+'.png')
 
 # we converting the image which is in PIL format into the numpy array, so that we can apply deep learning methods
 dataImage = img_to_array(image)
@@ -18,18 +21,23 @@ dataImage = img_to_array(image)
 imageNew = expand_dims(dataImage, 0)
 
 # now here below we creating the object of the data augmentation class
-imageDataGen = ImageDataGenerator(vertical_flip=True)
+imageDataGen_v = ImageDataGenerator(vertical_flip=True)
+imageDataGen_h = ImageDataGenerator(horizontal_flip=True)
 
 # because as we alreay load image into the memory, so we are using flow() function, to apply transformation
-iterator = imageDataGen.flow(imageNew, batch_size=1)
+iterator_v = imageDataGen_v.flow(imageNew, batch_size=1)
+iterator_h = imageDataGen_h.flow(imageNew, batch_size=1)
 
 # below we generate augmented images and plotting for visualization
-for i in range(5):
+for i in range(3):
     
     # generating images of each batch
-    batch = iterator.next()
-
+    batch_v = iterator_v.next()
+    batch_h = iterator_h.next()
+    
     # again we convert back to the unsigned integers value of the image for viewing
-    image = batch[0].astype('uint8')
+    imagev = batch_v[0].astype('uint8')
+    imageh = batch_h[0].astype('uint8')
 
-    save_img('v0_'+str(i)+'.png', image)
+    save_img(file_folder+'/'+file_name+'_v'+str(i)+'.png', imagev)
+    save_img(file_folder+'/'+file_name+'_h'+str(i)+'.png', imageh)
