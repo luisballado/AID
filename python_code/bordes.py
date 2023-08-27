2a5c2234b0c9317ca547778f0b2382033d31c6d5
Luis Ballado <luis@madlab.mx>
2023-08-27 01:42:39 -0600

    mod

--- python_code/bordes.py
+++ python_code/bordes.py
@@ -1,12 +1,33 @@
-
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('herramientas.png', cv.IMREAD_GRAYSCALE)
+
+img = cv.imread('todas_blanco2.jpg', cv.IMREAD_GRAYSCALE)
+
 assert img is not None, "file could not be read, check with os.path.exists()"
-edges = cv.Canny(img,100,200)
+
+im_floodfill = cv.Canny(img,100,200)
+
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+
+h, w = im_floodfill.shape[:2]
+mask = np.zeros((h+2, w+2), np.uint8)
+
+# Floodfill from point (0, 0)
+cv.floodFill(im_floodfill, mask, (0,0), 255);
+ 
+# Invert floodfilled image
+im_floodfill_inv = cv.bitwise_not(im_floodfill)
+
+# Combine the two images to get the foreground.
+im_out = im_floodfill_inv
+
+plt.subplot(122),plt.imshow(im_out,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
 plt.show()
+
+
+
+
