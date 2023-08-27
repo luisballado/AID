85b841488d75b7b6caeac4338f396b3b3648650c
Luis Ballado <luis@madlab.mx>
2023-08-26 23:23:34 -0600

    se agregar borrador de reporte

--- ejemplo_yolo/ejemplo.py
+++ ejemplo_yolo/ejemplo.py
@@ -4,5 +4,5 @@ from super_gradients.training import models
 device=torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 #model=models.get('yolo_nas_s',pretrained_weights="coco").to(device)
 model=models.get('yolo_nas_l',pretrained_weights="coco").to(device)
-out=model.predict("/Users/luisalbertoballadoaradias/Documents/ejemplo_yolo/todo.png")
+out=model.predict("/home/luis/Documents/cinvestav/FINAL/AID/ejemplo_yolo/todo.png")
 out.show()
